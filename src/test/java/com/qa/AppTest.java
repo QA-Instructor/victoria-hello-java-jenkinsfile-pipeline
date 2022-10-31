1fee78d01ed08d8711b2df52a179fdc609bffa89
Victoria <VictoriaLloyd@hotmail.com>
2022-10-31 16:16:39 +0000

    Initial Commit

---
+++ src/test/java/com/qa/AppTest.java
@@ -0,0 +1,30 @@
+package com.qa;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        assertFalse( false );
+    }
+}
+
+
